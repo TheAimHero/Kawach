70d7f1b7e5d55376d387f0cce93184c20a2cc59e
TheAimHero <vsghodekar1@gmail.com>
2023-08-01 01:14:38 +0530

    [feat] popup

--- src/html/popup.js
+++ src/html/popup.js
@@ -1,27 +1,26 @@
-const option = document.getElementById("option");
-const info = document.getElementById("info");
-const level = document.getElementById("level");
-
-const imgBlock = document.getElementById("imgBlock");
-const siteBlock = document.getElementById("siteBlock");
+const option = document.getElementById('option');
+const info = document.getElementById('info');
+// const level = document.getElementById('level');
+const imgBlock = document.getElementById('imgBlock');
+const siteBlock = document.getElementById('siteBlock');
 
 option.onclick = () => {
   chrome.runtime.openOptionsPage();
 };
 
 info.onclick = () => {
-  chrome.tabs.create({ url: "https://github.com/TheAimHero" });
+  chrome.tabs.create({ url: 'https://github.com/TheAimHero' });
 };
 
-chrome.storage.sync.get(["blockedSites", "blockedImg"], (items) => {
+chrome.storage.sync.get(['blockedSites', 'blockedImg'], items => {
   if (items.blockedSites) {
     siteBlock.innerHTML = items.blockedSites;
   } else {
-    siteBlock.innerHTML = "0";
+    siteBlock.innerHTML = '0';
   }
   if (items.blockedImg) {
     imgBlock.innerHTML = items.blockedImg;
   } else {
-    imgBlock.innerHTML = "0";
+    imgBlock.innerHTML = '0';
   }
 });
